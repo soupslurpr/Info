090858e11f7aeaf30de8441af403443c18b1f0f0
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2025-05-14 05:51:31 -0700

    replace MaterialTheme with MaterialExpressiveTheme

--- app/src/main/kotlin/app/grapheneos/info/ui/theme/Theme.kt
+++ app/src/main/kotlin/app/grapheneos/info/ui/theme/Theme.kt
@@ -1,8 +1,8 @@
 package app.grapheneos.info.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -32,6 +32,7 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun InfoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -49,7 +50,7 @@ fun InfoTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
